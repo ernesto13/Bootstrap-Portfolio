9ff0868d4d1e16195bb2193a14e43ea34f9bbdad
Ernesto Samaniego <esamaniego53@gmail.com>
2018-05-01 04:14:08 -0700

    added a working submit form

--- javascript/script.js
+++ javascript/script.js
@@ -5,4 +5,31 @@ $(document).ready(function() {
             width: '375px',
         })
     });
+
+    let name = document.getElementById("first_name");
+    let email = document.getElementById("email");
+    let message = document.getElementById("message");
+
+
+    $("#contact-form").submit("click", function(event) {
+
+        if (name.value === "" || email.value === "" || message.value === "") {
+
+            alertify.error('Fill out proper fields!');
+            event.preventDefault();
+
+        } else {
+            $.ajax({
+                url: "https://formspree.io/esamaniego53@gmail.com",
+                method: "POST",
+                data: $('#contact-form').serialize(),
+                dataType: "json"
+            });
+
+            $(this).get(0).reset()
+            alertify.success('Thank you for your time!');
+            event.preventDefault();
+
+        }
+    });
 });
